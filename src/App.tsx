dbbb0d4f9699381fa58dfa2fac9fd40c0c4c50de
n.ivanov <nikitadotmail@gmail.com>
2021-10-07 23:03:58 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from 'react';
+import { CSSTransition } from 'react-transition-group';
+import styles from './styles.module.css';
 
 function App() {
+  const [isShow, setIsShow] = useState<boolean>(false);
+  const [isFooter, setIsFooter] = useState<boolean>(false);
+  const footerRef: any = useRef(null);
+
+  useEffect(() => {
+    setIsShow(true);
+  }, []);
+
+  const onScroll = () => {
+    window.scrollY > 0 && setIsFooter(true);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className={styles.app}>
+      <CSSTransition
+        in={isShow}
+        timeout={0}
+        classNames={{
+          enterActive: styles.topEnter,
+          enterDone: styles.topEnterActive,
+        }}
+        // unmountOnExit
+        // mountOnEnter
+      >
+        <div className={styles.block} />
+      </CSSTransition>
+      <div ref={footerRef}>
+        <CSSTransition
+          in={isFooter}
+          timeout={0}
+          classNames={{
+            enterActive: styles.bottomEnter,
+            enterDone: styles.bottomEnterActive,
+          }}
+          unmountOnExit
+          mountOnEnter
         >
-          Learn React
-        </a>
-      </header>
+          <div className={styles.footer} />
+        </CSSTransition>
+      </div>
     </div>
   );
 }
